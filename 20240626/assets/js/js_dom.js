27a78c8d1f3f93f0d46971322782e1a39bc42528
PIN <a0968155661@gmail.com>
2024-06-26 19:02:33 +0800

    DOM 選取

---
+++ 20240626/assets/js/js_dom.js
@@ -0,0 +1,15 @@
+let onlyItem = document.querySelector('#only-item');
+console.log('單選唯一')
+console.log(onlyItem);
+
+let listItems = document.querySelector('#item-list li');
+console.log('單選多個')
+console.log(listItems);
+
+let listItems2 = document.querySelectorAll('#item-list li');
+console.log('多選多個')
+console.log(listItems2);
+
+let onlyItem2 = document.querySelectorAll('#only-item');
+console.log('多選唯一')
+console.log(onlyItem2);
--- 20240626/readme.md
+++ 20240626/readme.md
@@ -1,4 +1,19 @@
 # 20240626
 
-## 
+## DOM 單選
 
+只會取得第一個找到的DOM
+
+```js
+let selector = '#some';
+let dom = document.querySelector(selector);
+```
+
+## DOM 多選
+
+不管找到幾個，都使用NodeList陣列包裝
+
+```js
+let selector = '#some';
+let items = document.querySelectorAll(selector);
+```
